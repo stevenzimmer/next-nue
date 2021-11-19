713373b5fbdaa8ee24f27f46f22d20dc6aeb7cc6
Steven Zimmer <webdevzim@gmail.com>
2021-11-18 17:07:54 -0800

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -13,17 +13,17 @@ export default function Footer() {
                                         nue
                                     </a>
                                 </Link>
-                                <div className="px-12 flex items-center justify-start">
+                                <div className="px-4 md:px-12 flex items-center justify-start">
                                     <div className="px-6">
                                         <Link href="/about/">
-                                            <a className="text-lg md:text-xl text-white opacity-80 hover:opacity-100 transition-opacity duration-200 ease-linear">
+                                            <a className="text-sm md:text-base text-white opacity-80 hover:opacity-100 transition-opacity duration-200 ease-linear">
                                                 About
                                             </a>
                                         </Link>
                                     </div>
                                     <div className="px-6">
                                         <Link href="#">
-                                            <a className="text-lg md:text-xl text-white opacity-80 hover:opacity-100 transition-opacity duration-200 ease-linear">
+                                            <a className="text-sm md:text-base text-white opacity-80 hover:opacity-100 transition-opacity duration-200 ease-linear">
                                                 Hiring
                                             </a>
                                         </Link>
@@ -31,7 +31,7 @@ export default function Footer() {
                                 </div>
                             </div>
 
-                            <div className="text-center text-white">
+                            <div className="text-right text-white">
                                 <p>&#169; {new Date().getFullYear()}</p>
                             </div>
                         </div>
