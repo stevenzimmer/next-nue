9661ed1b0d72f800c95e48649205de025a151ceb
Steven Zimmer <webdevzim@gmail.com>
2021-12-03 13:50:36 -0800

    content updates

--- components/text/Header1.js
+++ components/text/Header1.js
@@ -1,6 +1,6 @@
 export default function Header1({ children }) {
     return (
-        <h1 className="text-3xl md:text-4xl lg:text-6xl font-hntMedium font-medium">
+        <h1 className="text-3xl md:text-4xl lg:text-6xl font-hntMedium">
             {children}
         </h1>
     );
