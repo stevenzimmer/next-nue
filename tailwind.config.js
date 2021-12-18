17d59d163d9aae441c14320420ed3b6feb234435
Steven Zimmer <webdevzim@gmail.com>
2021-12-17 16:02:49 -0800

    data folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
         "./layouts/**/*.{js,ts,jsx,tsx}",
+        "./data/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
