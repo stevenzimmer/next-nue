aaf1f66aa542edf9e3ad063de7918a1c05ff14fc
EricSnowPeak <eric.liu@getruby.io>
2022-03-09 10:37:28 +1300

    Integrated with GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import React from "react";
 import { AuthProvider } from "@/context/AuthContext";
 import "../styles/globals.css";
 import Layout from "@/components/Layout";
+import TagManager from "react-gtm-module";
 
 function NueApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    if (process.env.NODE_ENV === 'production' && process.env.GTM_ID) {
+      const tagManagerArgs = {
+        id: process.env.GTM_ID,
+      }
+      TagManager.initialize(tagManagerArgs)
+    } else {
+      console.log('GTM not initialized')
+    }
+  }, [])
+
     return (
         <AuthProvider>
             <Layout
