221df286d160db077e2017023306f8056e4c3ccc
EricSnowPeak <eric.liu@getruby.io>
2022-03-09 19:59:28 +1300

    Remove info print

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     trailingSlash: true,
+    // https://github.com/aws-amplify/amplify-hosting/issues/1987#issuecomment-885217940
     env: {
         BUILD_ENV: process.env.BUILD_ENV,
         GTM_ID: process.env.GTM_ID,
--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,11 @@ import TagManager from "react-gtm-module";
 function NueApp({ Component, pageProps }) {
   React.useEffect(() => {
     if (process.env.BUILD_ENV === 'prod' && process.env.GTM_ID) {
-      console.log('GTM init')
       const tagManagerArgs = {
         id: process.env.GTM_ID,
       }
       TagManager.initialize(tagManagerArgs)
-    } else {
-      console.log('GTM not initialized')
     }
-    console.log('env', process.env.BUILD_ENV)
   }, [])
 
     return (
