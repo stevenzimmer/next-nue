c8a914f912aee9ab30f9251e7797de148c70b601
Steven Zimmer <webdevzim@gmail.com>
2023-12-02 06:27:28 -0800

    Back link

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import { AuthProvider } from "@/context/AuthContext";
 import "../styles/globals.css";
 import Layout from "@/components/Layout";
+import link from "next/link";
+import { Link } from "react-scroll";
 
 function MdrnApp({ Component, pageProps }) {
     return (
@@ -13,6 +15,9 @@ function MdrnApp({ Component, pageProps }) {
                 }}
             >
                 <Component {...pageProps} />
+                
+                <a  className="block fixed left-6 bottom-6 z-50 bg-white rounded-lg px-12 py-3 shadow-lg border-2 border-transparent hover:border-blue-200 hover:bg-blue-50 font-hntMedium" href="https://webzim.dev/">&#8592; &nbsp; Back to Webzim</a>
+                
             </Layout>
         </AuthProvider>
     );
