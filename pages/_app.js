0840304e3d538115e45e079055b95c7e1bf76e7d
Steven Zimmer <webdevzim@gmail.com>
2023-12-04 19:26:29 -0800

    spacing update

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MdrnApp({ Component, pageProps }) {
             >
                 <Component {...pageProps} />
                 
-                <a  className="block fixed left-6 bottom-6 z-50 bg-white rounded-lg px-12 py-3 shadow-lg border-2 border-transparent hover:border-blue-200 hover:bg-blue-50 font-hntMedium" href="https://webzim.dev/">&#8592; &nbsp; Back to Webzim</a>
+                <a className="block fixed left-6 bottom-6 z-50 bg-white rounded-lg px-12 py-3 shadow-lg border-2 border-transparent hover:border-blue-200 hover:bg-blue-50 font-hntMedium" href="https://webzim.dev/">&#8592; &nbsp; Back to Webzim</a>
                 
             </Layout>
         </AuthProvider>
